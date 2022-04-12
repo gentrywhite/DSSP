5c35a0960b53079926eb4633bcb3a1d837f91e19
Rex Parsons <47549264+RWParsons@users.noreply.github.com>
2022-04-12 17:31:06 +1000

    add covariate summary to summary.dsspMod

--- R/summary.R
+++ R/summary.R
@@ -25,7 +25,7 @@ summary.dsspMod <- function(object, prob = 0.95, robust = FALSE, mc_se = FALSE, 
     }
     measures$Est.Error <- stats::sd
   }
-  measures <- c(measures, list(
+  full_summary_measures <- c(measures, list(
     ll = function(x) stats::quantile(x, probs=probs[1]),
     ul = function(x) stats:: quantile(x, probs=probs[2]),
     Rhat = posterior::rhat,
@@ -33,7 +33,7 @@ summary.dsspMod <- function(object, prob = 0.95, robust = FALSE, mc_se = FALSE, 
     Tail_ESS = posterior::ess_tail
   ))
   
-  full_summary <- lapply(measures, function(m) sapply(variables, function(v) m(v)))
+  full_summary <- lapply(full_summary_measures, function(m) sapply(variables, function(v) m(v)))
   full_summary <- as.data.frame(full_summary)
   
   prob <- probs[2] - probs[1]
@@ -42,6 +42,26 @@ summary.dsspMod <- function(object, prob = 0.95, robust = FALSE, mc_se = FALSE, 
   rownames(full_summary) <- names(variables)
   
   out <- c(out, list(full_summary=full_summary))
+  if(!is.null(object$covariates_posterior)){
+    cov_measures <- c(measures, list(
+      min = min,
+      `q0.025` = function(x) stats::quantile(x, probs=0.025),
+      `q0.25` = function(x) stats::quantile(x, probs=0.25),
+      `q0.50` = function(x) stats::quantile(x, probs=0.50),
+      `q0.75` = function(x) stats::quantile(x, probs=0.75),
+      `q0.975` = function(x) stats::quantile(x, probs=0.975),
+      max = max
+    ))
+    
+    cov_list <- stats::setNames(
+      split(object$covariates_posterior, seq(nrow(object$covariates_posterior))),
+      rownames(object$covariates_posterior)
+    )
+    
+    cov_summary <- lapply(cov_measures, function(m) sapply(cov_list, function(v) m(v)))
+    cov_summary <- as.data.frame(cov_summary)
+    out <- c(out, list(cov_summary=cov_summary))
+  }
   class(out) <- "dsspModsummary"
   out
 }
@@ -53,4 +73,8 @@ print.dsspModsummary <- function(x, digits = 2, ...) {
   cat(paste0("Number of iterations: ", x$niter, "\n\n"))
   cat("Summary of model:\n")
   print_format(x$full_summary)
+  if(!is.null(x$cov_summary)){
+    cat("\nSummary of covariates:\n")
+    print_format(x$cov_summary)
+  }
 }
