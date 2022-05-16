20bcebad800d576d7f1cbeaec6189d7d1c60d5b0
Rex Parsons <47549264+RWParsons@users.noreply.github.com>
2022-05-16 15:30:42 +1000

    clean up references to dependencies for plot

--- R/plot.R
+++ R/plot.R
@@ -9,11 +9,10 @@
 #'   Used for interpolation (\code{akime::interp()}). 
 #' @param ny dimension of output grid in y direction.
 #'   Used for interpolation (\code{akime::interp()}). 
-#' @param pal colour palette used for filled contour plot.
-#' @param nlevels number of levels used in contour plots.
+#' @param nlevels number of levels used in contour plot.
 #' @param ... additional arguments that are passed to \code{ggplot2::scale_fill_distiller()}.
 #'
-#' @return NULL
+#' @return a list containing the plots printed (individually and together in grid)
 #' @export 
 #'
 #' @examples
@@ -42,49 +41,49 @@ plot.dsspMod <- function(x,
   metric <- ifelse(robust_residuals, stats::median, mean)
   yh <- apply(ypred, 1, metric)
   y <- reverse_scaling(x$Y, x$y_scaling)
-  
+  n <- eta <- delta <- z <- values <- NULL
   df_params <- data.frame(eta=x$eta, delta=x$delta)
   df_params$n <- seq.int(nrow(df_params))
   
   resid_vs_fitted <- 
-    ggplot() +
-    geom_point(aes(x=yh, y=r)) +
-    geom_smooth(aes(x=yh, y=r), se=FALSE, col="black") +
-    labs(
+    ggplot2::ggplot() +
+    ggplot2::geom_point(ggplot2::aes(x=yh, y=r)) +
+    ggplot2::geom_smooth(ggplot2::aes(x=yh, y=r), se=FALSE, col="black", method="loess") +
+    ggplot2::labs(
       title="Residuals vs fitted values",
       x=paste("fitted", x$dep_var),
       y="residuals"
     )
 
   predicted_vs_actual <-
-    ggplot() +
-    geom_point(aes(y, yh)) +
-    geom_abline(slope=1, intercept=0) +
-    labs(
+    ggplot2::ggplot() +
+    ggplot2::geom_point(ggplot2::aes(y, yh)) +
+    ggplot2::geom_abline(slope=1, intercept=0) +
+    ggplot2::labs(
       title="Observe vs fitted values",
       x=paste("observed", x$dep_var),
       y=paste("fitted", x$dep_var)
     )
 
   eta_density <- 
-    ggplot() +
-    geom_density(aes(x$eta)) +
-    labs(title=expression("Posterior Density of " * eta), x=expression(eta))
+    ggplot2::ggplot() +
+    ggplot2::geom_density(ggplot2::aes(x$eta)) +
+    ggplot2::labs(title=expression("Posterior Density of " * eta), x=expression(eta))
   
   eta_trace <-
-    ggplot(data=df_params) +
-    geom_line(aes(x=n, y=eta)) +
-    labs(title=expression("Traceplot of " * eta), y=expression(eta))
+    ggplot2::ggplot(data=df_params) +
+    ggplot2::geom_line(ggplot2::aes(x=n, y=eta)) +
+    ggplot2::labs(title=expression("Traceplot of " * eta), y=expression(eta))
   
   delta_density <-
-    ggplot() +
-    geom_density(aes(x$delta)) +
-    labs(title=expression("Posterior Density of " * delta), x=expression(delta))
+    ggplot2::ggplot() +
+    ggplot2::geom_density(ggplot2::aes(x$delta)) +
+    ggplot2::labs(title=expression("Posterior Density of " * delta), x=expression(delta))
   
   delta_trace <-
-    ggplot(data=df_params) +
-    geom_line(aes(x=n, y=delta)) +
-    labs(title=expression("Traceplot of " * delta), y=expression(delta))
+    ggplot2::ggplot(data=df_params) +
+    ggplot2::geom_line(ggplot2::aes(x=n, y=delta)) +
+    ggplot2::labs(title=expression("Traceplot of " * delta), y=expression(delta))
   
   diagnostic_plots <- list(
     resid_vs_fitted=resid_vs_fitted,
@@ -97,31 +96,31 @@ plot.dsspMod <- function(x,
   
   if(contour_plots){
     interp_y <- akima::interp(x$coords[,1], x$coords[,2], y, nx=nx, ny=ny)
-    interp_df <- na.omit(as.data.frame(akima::interp2xyz(interp_y)))
+    interp_df <- stats::na.omit(as.data.frame(akima::interp2xyz(interp_y)))
     
     contour <- 
-      ggplot(data=interp_df, aes(x=x, y=y, z=z)) +
-      geom_contour(col="black", bins=nlevels) +
-      labs(
+      ggplot2::ggplot(data=interp_df, ggplot2::aes(x=x, y=y, z=z)) +
+      ggplot2::geom_contour(col="black", bins=nlevels) +
+      ggplot2::labs(
         x=colnames(x$coords)[1],
         y=colnames(x$coords)[2],
         title="Contour plot"
       )
     
     filled_contour <- 
-      ggplot(data=interp_df, aes(x=x, y=y, fill=z)) +
-      geom_tile() + 
-      scale_fill_distiller(...) +
-      labs(
+      ggplot2::ggplot(data=interp_df, ggplot2::aes(x=x, y=y, fill=z)) +
+      ggplot2::geom_tile() + 
+      ggplot2::scale_fill_distiller(...) +
+      ggplot2::labs(
         x=colnames(x$coords)[1],
         y=colnames(x$coords)[2],
         title="Filled contour plot",
         fill=x$dep_var
       ) +
-      theme(legend.position = "bottom")
+      ggplot2::theme(legend.position = "bottom")
     
     legend <- cowplot::get_legend(filled_contour)
-    filled_contour_no_legend <- filled_contour + theme(legend.position = "none")
+    filled_contour_no_legend <- filled_contour + ggplot2::theme(legend.position = "none")
     
     diagnostic_plots_grid <- c(
       diagnostic_plots, 
@@ -133,12 +132,12 @@ plot.dsspMod <- function(x,
     )
     
     plots <- cowplot::plot_grid(plotlist=diagnostic_plots_grid, ncol=2)
-    grid1 <- cowplot::plot_grid(plots, legend, rel_heights=c(1,0.12), ncol=1)
+    diagnostic_grid <- cowplot::plot_grid(plots, legend, rel_heights=c(1,0.12), ncol=1)
   } else {
     diagnostic_plots_grid <- diagnostic_plots_return <- diagnostic_plots
-    grid1 <- cowplot::plot_grid(plotlist=diagnostic_plots_grid, ncol=2)
+    diagnostic_grid <- cowplot::plot_grid(plotlist=diagnostic_plots_grid, ncol=2)
   }
-  print(grid1)
+  print(diagnostic_grid)
   
   covariates_plots <- list()
   for(i in 1:nrow(x$covariates_posterior)) {
@@ -147,32 +146,31 @@ plot.dsspMod <- function(x,
     df_i$n <- seq.int(nrow(df_i))
     
     trace_plot <- 
-      ggplot(data=df_i, aes(x=n, y=values)) +
-      geom_line() +
-      labs(y=cov_name)
+      ggplot2::ggplot(data=df_i, ggplot2::aes(x=n, y=values)) +
+      ggplot2::geom_line() +
+      ggplot2::labs(y=cov_name)
     
     density_plot <- 
-      ggplot(data=df_i) +
-      geom_density(aes(x=values)) +
-      labs(x=cov_name)
+      ggplot2::ggplot(data=df_i) +
+      ggplot2::geom_density(ggplot2::aes(x=values)) +
+      ggplot2::labs(x=cov_name)
     
     new_plots <- list(density_plot=density_plot, trace_plot=trace_plot)
     names(new_plots) <- paste(cov_name, names(new_plots), sep="_")
     
     covariates_plots <- c(covariates_plots, new_plots)
   }
-  grid2 <- cowplot::plot_grid(plotlist=covariates_plots, ncol=2)
-  print(grid2)
+  covariates_grid <- cowplot::plot_grid(plotlist=covariates_plots, ncol=2)
+  print(covariates_grid)
   
   res <- list(
     diagnostic_plots = diagnostic_plots_return,
     covariates_plots = covariates_plots,
-    diagnostic_grid = grid1,
-    covariates_grid = grid2
+    diagnostic_grid = diagnostic_grid,
+    covariates_grid = covariates_grid
   )
-  return(invisible(res))
   
-  
+  invisible(res)
 }
 
 
